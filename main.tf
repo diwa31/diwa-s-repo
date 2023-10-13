8972b682c30d12ad16374fefcc3cad5fe3f3b258
N Neeharika Reddy <n.neeharikareddy@gmail.com>
2023-10-13 07:06:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,7 @@ resource "aws_s3_bucket" "bucket1" {
 
 
 provider "aws" {
-    access_key=var.accesskey
-    secret_key=var.secretkey
     region = "eu-west-1"
 }
 
-variable "accesskey"{
-    default="AKIAZTIMJ7JHDTEXKPB3"
 
-}
-
-variable "secretkey"{
-    default="MQrsMUvEVEL98/gOzMrFfgpxKb+bCw7ttTWGMgee"
-}
